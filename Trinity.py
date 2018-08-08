0ff3ddd793d0f285de6f9f08588c4111807f2130
Guest1300 <42157557+Guest1300@users.noreply.github.com>
2018-08-08 16:09:41 -0400

    Added Pylons & Probes

--- Trinity.py
+++ Trinity.py
@@ -4,7 +4,7 @@ from sc2.data import race_gas, race_worker, race_townhalls, ActionResult, Attrib
 
 import sc2  # pip install sc2
 from sc2 import Race, Difficulty, run_game, maps
-from sc2.constants import *  # for autocomplete
+from sc2.constants import *  # for autocompletepomg spurce code
 from sc2.ids.unit_typeid import *
 from sc2.ids.ability_id import *
 from sc2.position import Point2, Point3
@@ -18,13 +18,22 @@ import random
 class Trinity(sc2.BotAI):
     def __init__(self):
         self.myRace = "None"
+        self.stopWorker = False
 
     async def on_step(self, iteration):
         if iteration == 0:
             await self.onStart()
-        self.remember_enemy_units()
-        self.remember_friendly_units()
+        # self.remember_enemy_units()
+        # self.remember_friendly_units()
         await self.distribute_workers()
+
+        if self.myRace == "Terran":
+            print(self.myRace)
+        elif self.myRace == "Zerg":
+           print(self.myRace)
+        elif self.myRace == "Protoss":
+            await self.buildPylons()
+            await self.buildProbes()
 
     async def onStart(self):
         await self.chat_send("(glhf)")
@@ -34,6 +43,20 @@ class Trinity(sc2.BotAI):
             self.myRace = "Zerg"
         elif self.townhalls == self.units(NEXUS):
             self.myRace = "Protoss"
+
+    async def buildProbes(self):
+        if (self.units(NEXUS).amount * 22) > self.units(PROBE).amount:
+            if self.units(PROBE).amount < 70 and self.can_afford(PROBE):
+                for nexus in self.units(NEXUS).noqueue:
+                    await self.do(nexus.train(PROBE))
+
+    async def buildPylons(self):
+        if self.units(NEXUS).exists:
+            nexus = self.units(NEXUS).random
+            if self.supply_left <= 8 and self.already_pending(PYLON) < 2 and self.supply_cap < 200:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexus.position.towards(self.game_info.map_center, 8))
+
 
     def remember_enemy_units(self):
         # Every 60 seconds, clear all remembered units (to clear out killed units)
@@ -77,9 +100,9 @@ class Trinity(sc2.BotAI):
 
             self.remembered_friendly_units_by_tag[unit.tag] = unit
 
-run_game(maps.get("MechDepotLE"), [
+run_game(maps.get("(2)CatalystLE"), [
     # Human(Race.Zerg),
-    Bot(Race.Random, Trinity()),
+    Bot(Race.Protoss, Trinity()),
     # Bot(Race.Protoss, CannonLoverBot())
     Computer(Race.Zerg, Difficulty.VeryHard)
 ], realtime=True)
