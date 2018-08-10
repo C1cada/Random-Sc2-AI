6ac3582c002f9b1dd80a45358be2b6268c175acb
Guest1300 <42157557+Guest1300@users.noreply.github.com>
2018-08-10 10:34:33 -0400

    Added More Functions
    trainZealotsandStalkers, etc.

--- Trinity.py
+++ Trinity.py
@@ -67,6 +67,53 @@ class Trinity(sc2.BotAI):
             if self.supply_left <= 8 and self.already_pending(PYLON) < 2 and self.supply_cap < 200:
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexus.position.towards(self.game_info.map_center, 8))
+                    
+     async def buildGateway(self):
+        if self.units(PYLON).exists and self.can_afford(GATEWAY):
+            if self.units(GATEWAY).amount < 2* self.townhalls.amount and self.units(GATEWAY).amount < 6:
+                pylon = self.units(PYLON).random
+                await self.build(GATEWAY, near=pylon.position.towards(self.game_info.map_center))
+    async def buildCyber(self):
+        if self.units(PYLON).exists and self.units(GATEWAY).exists and self.can_afford(CYBERNETICSCORE):
+            if not self.units(CYBERNETICSCORE).exists and not self.already_pending(CYBERNETICSCORE):
+                pylon = self.units(PYLON).random
+                await self.build(CYBERNETICSCORE, near=pylon)
+
+    async def buildAssimilator(self):
+        if self.townhalls.exists:
+            hq = self.townhalls.random
+            vaspenes = self.state.vespene_geyser.closer_than(15.0, hq)
+            if self.townhalls.amount < 6:
+                if not self.already_pending(ASSIMILATOR) and (self.units(ASSIMILATOR).amount / (self.townhalls.amount * 1.5)) < 1:
+                    for vaspene in vaspenes:
+                        if not self.can_afford(ASSIMILATOR):
+                            break
+                        worker = self.select_build_worker(vaspene.position)
+                        if worker is None:
+                            break
+                        if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                            await self.do(worker.build(ASSIMILATOR, vaspene))
+            elif self.townhalls.amount > 5:
+                if not self.already_pending(ASSIMILATOR):
+                    for vaspene in vaspenes:
+                        if not self.can_afford(ASSIMILATOR):
+                            break
+                        worker = self.select_build_worker(vaspene.position)
+                        if worker is None:
+                            break
+                        if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                            await self.do(worker.build(ASSIMILATOR, vaspene))
+
+    async def trainZealotsAndStalkers(self):
+        if self.units(GATEWAY).exists :
+            for gt in self.units(GATEWAY).noqueue:
+                if self.units(CYBERNETICSCORE).exists:
+                    if self.units(STALKER).amount *2 <  self.units(ZEALOT).amount and self.can_afford(STALKER):
+                        await self.do(gt.train(STALKER))
+                    elif self.can_afford(ZEALOT):
+                        await self.do(gt.train(ZEALOT))
+                elif self.can_afford(ZEALOT):
+                    await self.do(gt.train(ZEALOT))
     ###TERRAN FUNCTIONS###
      async def buildSCVs(self):
         if (self.townhalls.amount * 22) > self.units(SCV).amount:
@@ -80,9 +127,27 @@ class Trinity(sc2.BotAI):
             if self.supply_left <= 8 and self.already_pending(SUPPLYDEPOT) < 2 and self.supply_cap < 200:
                 if self.can_afford(SUPPLYDEPOT):
                     await self.build(SUPPLYDEPOT, near=cc.position.towards(self.game_info.map_center, 8))
+    
+     async def buildBarracks(self):
+        if self.units(SUPPLYDEPOT).exists and self.can_afford(BARRACKS) and self.townhalls.exists:
+            if self.units(BARRACKS).amount < 2 * self.townhalls.amount and self.units(BARRACKS).amount < 6:
+                cc = self.townhalls.random
+                await self.build(BARRACKS, near=cc.position.towards(self.game_info.map_center, 8))
+
+    async def trainMarine(self):
+        if self.units(BARRACKS).exists and self.can_afford(MARINE):
+            for br in self.units(BARRACKS).noqueue:
+                await self.do(br.train(MARINE))
 
     ###ZERG FUNCTIONS###
     
+     async def buildDrones(self):
+        if self.units(LARVA).exists:
+            for larva in self.units(LARVA):
+                if self.already_pending(DRONE) <= 4 and self.units(DRONE).amount < 80:
+                    if self.units(DRONE).amount < 22 * self.townhalls.amount and self.can_afford(DRONE):
+                        await self.do(larva.train(DRONE))
+
     ###USE FUNCTIONS###
 run_game(maps.get("(2)CatalystLE"), [
     # Human(Race.Zerg),
